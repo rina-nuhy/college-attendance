63d05525921b6299cba9a06836bfa582ff603f10
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-12-11 14:52:58 +0300

    made some change

--- attendance/models.py
+++ attendance/models.py
@@ -6,30 +6,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
-
-# class CustomUserManager(UserManager):
-#     def _create_user(self, email, password, **extra_fields):
-#         email = self.normalize_email(email)
-#         user = CustomUser(email=email, **extra_fields)
-#         user.password = make_password(password)
-#         user.save(using=self._db)
-#         return user
-
-#     def create_user(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault("is_staff", False)
-#         extra_fields.setdefault("is_superuser", False)
-#         return self._create_user(email, password, **extra_fields)
-
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault("is_staff", True)
-#         extra_fields.setdefault("is_superuser", True)
-
-#         assert extra_fields["is_staff"]
-#         assert extra_fields["is_superuser"]
-#         return self._create_user(email, password, **extra_fields)
-
-
 class Session(models.Model):
      start_year = models.DateField()
      end_year = models.DateField()
